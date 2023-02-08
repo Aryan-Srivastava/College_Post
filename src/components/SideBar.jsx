402d46db97d53f5918bc54f9435064b94498cc52
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-02-09 00:00:37 +0530

    Made the website mobile responsive

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,6 +2,8 @@
 import { HashTags } from "./index";
 import "../index.css";
 import { Link } from "react-router-dom";
+import sidebar_logo from "./sidebar_logo.png";
+import { useState } from "react";
 // import axios from "axios";
 // import { Link } from "react-router-dom";
 
@@ -16,64 +18,171 @@ const SideBar = () => {
   //   getCats();
   // })
 
+  const [enable, setEnable] = useState(false);
+
   return (
-    <aside className=" flex-[3_3_0] w-54 ml-8 mt-24 " aria-label="Sidebar">
-      <div className="overflow-y-auto">
-        <div>
-          <div className="flex mt-5 mb-5">
-            <h1 className="text-2xl w-[40%] font-bold text-gray-600"> <span className="text-orange-600">T</span>ag Cloud</h1>
-            <hr className=" ml-3 mt-4 border-orange-600 bg-orange-600 border-y w-72 " />
-          </div>
-          <ul className="flex text-sm flex-wrap mt-2">
-            {/* {cats.map((c) => (
+    <>
+      <div
+        onClick={() => setEnable(!enable)}
+        className=" inline-block top-32 p-4 w-[50px] cursor-pointer lg:hidden"
+      >
+        <img
+          className="top-[205px] absolute w-[50px]"
+          src={sidebar_logo}
+          alt=""
+        />
+      </div>
+
+      <aside
+        className={
+          enable
+            ? "fixed bg-[#fbe3e3] w-full top-[60px] p-[50px] z-10"
+            : " flex-[3_3_0] w-54 ml-8 mt-24 hidden lg:block"
+        }
+        aria-label="Sidebar"
+      >
+        <div className="overflow-y-auto">
+          <div>
+            <div className="flex mt-5 mb-5">
+              <h1
+                className={
+                  enable
+                    ? "text-2xl  font-bold ml-[20px] w-[20%] text-gray-600"
+                    : "text-2xl w-[40%] font-bold text-gray-600"
+                }
+              >
+                {" "}
+                <span className="text-orange-600 ">T</span>ag Cloud
+              </h1>
+              <div
+                onClick={() => setEnable(!enable)}
+                className=" inline-block top-32 p-4 w-[50px] cursor-pointer lg:hidden"
+              >
+                <img
+                  className="top-[16px] right-[16px] rotate-180 absolute w-[50px] "
+                  src={sidebar_logo}
+                  alt=""
+                />
+              </div>
+              <hr
+                className={
+                  enable
+                    ? " ml-[-44px] mt-4  border-orange-600 bg-orange-600 border-y w-[80vw] "
+                    : " ml-3 mt-4 border-orange-600 bg-orange-600 border-y w-72 "
+                }
+              />
+            </div>
+            <ul className="flex flex-row text-lg flex-wrap mt-2">
+              {/* {cats.map((c) => (
               <Link to={`/?cat=${c.name}`} key={c._id}>
                 <HashTags key={c._id} name={c.name} />
               </Link>
             ))} */}
-            <HashTags color="text-purple-400" name="Get Started"/>
-            <HashTags color="text-teal-400" name="Tech"/>
-            <HashTags color="text-lime-400" name="Music"/>
-            <HashTags color="text-green-400" name="Life"/>
-            <HashTags color="text-yellow-400" name="Notice"/>
-            <HashTags color="text-blue-400" name="Health"/>
-            <HashTags color="text-cyan-400" name="Travel"/>
-            <HashTags color="text-red-300" name="Important" />
+              <HashTags
+                className="text-[20px]"
+                color="text-purple-400"
+                name="Get Started"
+              />
+              <HashTags
+                className="text-[20px]"
+                color="text-teal-400"
+                name="Tech"
+              />
+              <HashTags
+                className="text-[20px]"
+                color="text-lime-400"
+                name="Music"
+              />
+              <HashTags
+                className="text-[20px]"
+                color="text-green-400"
+                name="Life"
+              />
+              <HashTags
+                className="text-[20px]"
+                color="text-yellow-400"
+                name="Notice"
+              />
+              <HashTags
+                className="text-[20px]"
+                color="text-blue-400"
+                name="Health"
+              />
+              <HashTags
+                className="text-[20px]"
+                color="text-cyan-400"
+                name="Travel"
+              />
+              <HashTags
+                className="text-[20px]"
+                color="text-red-300"
+                name="Important"
+              />
+            </ul>
+          </div>
+          <div className="flex mt-5 mb-5">
+            <h1
+              className={
+                enable
+                  ? "text-2xl ml-[20px] w-[20%] tag-cloud font-bold text-gray-600"
+                  : "text-2xl w-[30%] tag-cloud font-bold text-gray-600 "
+              }
+            >
+              <span className="text-orange-600">F</span>ollow !
+            </h1>
+            <hr
+              className={
+                enable
+                  ? " ml-3 mt-4 border-orange-600 bg-orange-600 border-y w-[80vw] "
+                  : " ml-3 mt-4 h-[0.15rem] border-orange-600 bg-orange-600 border-y w-64 "
+              }
+            />
+          </div>
+          <ul className="flex justify-evenly overflow-hidden">
+            <li>
+              <Link className="sidebar-social-icon">
+                <i className="fa-brands fa-instagram px-5 text-[#f009a5] duration-200 hover:scale-125"></i>
+              </Link>
+            </li>
+            <li>
+              <Link className="sidebar-social-icon">
+                <i className="fa-brands fa-facebook-f px-5 text-[#175beb] duration-200 hover:scale-125"></i>
+              </Link>
+            </li>
+            <li>
+              <Link className="sidebar-social-icon">
+                <i className="fa-brands fa-twitter px-5 text-[#1da1f2] duration-200 hover:scale-125"></i>
+              </Link>
+            </li>
+            <li>
+              <Link className="sidebar-social-icon">
+                <i className="fa-solid fa-envelope px-5 text-[#ff6f6f] duration-200 hover:scale-125"></i>
+              </Link>
+            </li>
           </ul>
         </div>
-        <div className="flex mt-5 mb-5">
-          <h1 className="text-2xl w-[30%] tag-cloud font-bold text-gray-600"><span className="text-orange-600">F</span>ollow !</h1>
-          <hr className=" ml-3 mt-4 h-[0.15rem] border-orange-600 bg-orange-600 border-y w-64 " />
+        <div>
+          <div className="flex mt-5 mb-5">
+            <h1
+              className={
+                enable
+                  ? "text-2xl ml-[20px] w-[20%] tag-cloud font-bold text-gray-600"
+                  : "text-2xl w-[40%] tag-cloud font-bold text-gray-600"
+              }
+            >
+              <span className="text-orange-600">A</span>dvertising
+            </h1>
+            <hr
+              className={
+                enable
+                  ? "ml-3 mt-4 border-orange-600 bg-orange-600 border-y w-[80vw]  "
+                  : " ml-3 mt-4 border-orange-600 bg-orange-600 border-y w-56 "
+              }
+            />
+          </div>
         </div>
-        <ul className="flex justify-evenly overflow-hidden">
-          <li>
-            <Link className="sidebar-social-icon">
-              <i className="fa-brands fa-instagram px-5 text-[#f009a5] duration-200 hover:scale-125"></i>
-            </Link>
-          </li>
-          <li>
-            <Link className="sidebar-social-icon">
-              <i className="fa-brands fa-facebook-f px-5 text-[#175beb] duration-200 hover:scale-125"></i>
-            </Link>
-          </li>
-          <li>
-            <Link className="sidebar-social-icon">
-              <i className="fa-brands fa-twitter px-5 text-[#1da1f2] duration-200 hover:scale-125"></i>
-            </Link>
-          </li>
-          <li>
-            <Link className="sidebar-social-icon">
-              <i className="fa-solid fa-envelope px-5 text-[#ff6f6f] duration-200 hover:scale-125"></i>
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <div className="flex mt-5 mb-5">
-          <h1 className="text-2xl w-[40%] tag-cloud font-bold text-gray-600"><span className="text-orange-600">A</span>dvertising</h1>
-          <hr className=" ml-3 mt-4 border-orange-600 bg-orange-600 border-y w-56 " />
-        </div>
-      </div>
-    </aside>
+      </aside>
+    </>
   );
 };
 
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 const TopBar = () => {
+  const [enable, setEnable] = useState(false);
   return (
-    <nav className="fixed w-[100%] z-50 border-b backdrop-filter backdrop-blur-lg bg-opacity-30 bg-white  px-2 sm:px-4 py-2.5 ">
-      <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <Link className="flex items-center">
+    <nav
+      className={
+        enable
+          ? "bg-slate-300 opacity-100 z-[2] w-full fixed h-[79px] justify-center text-center"
+          : "fixed w-[100%] z-50 border-b backdrop-filter backdrop-blur-lg bg-opacity-30 bg-white  px-2 sm:px-4 py-2.5 h-fit justify-center"
+      }
+    >
+      <div
+        className={
+          enable
+            ? "container flex flex-wrap items-center  justify-center mx-auto"
+            : "container flex flex-wrap items-center  justify-between mx-auto"
+        }
+      >
+        <div className="flex justify-center items-center lg:order-2">
+          <div
+            onClick={() => setEnable(!enable)}
+            className="hamburger z-20 inline-block p-4 absolute left-[3px] cursor-pointer lg:hidden"
+          >
+            <div
+              className={
+                enable
+                  ? "line transition-all  h-[0.3rem] w-7 my-1 rotate-45  bg-black"
+                  : "line transition-all  h-1 w-6 my-1 bg-black"
+              }
+            ></div>
+            <div
+              className={
+                enable
+                  ? "line transition-all  h-[0.3rem] w-7 my-1 -translate-y-[8px] -rotate-45 bg-black"
+                  : "line transition-all  h-1 w-6 my-1 bg-black"
+              }
+            ></div>
+            <div
+              className={enable ? "hidden" : "line  h-1 w-6 my-1 bg-black"}
+            ></div>
+          </div>
+          {/* <div className="search lg:hidden">
+            <img src="/search.svg" className="w-6" alt="" />
+          </div> */}
+        </div>
+
+        <Link className={enable ? "hidden" : "flex items-center "}>
           <span className=" text-xl font-bold whitespace-nowrap">
             <span className="text-orange-600">C</span>ollege Post
           </span>
         </Link>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="nav-middle-ul">
+        <div
+          className={
+            enable ? "flex flex-row pt-1" : "hidden w-full md:block md:w-auto"
+          }
+          id="navbar-default"
+        >
+          <ul
+            className={
+              enable
+                ? "flex flex-row font-semibold text-[25px]"
+                : "nav-middle-ul"
+            }
+          >
             <li>
               <Link
                 className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:text-orange-600 md:p-0"
@@ -23,9 +75,7 @@ const TopBar = () => {
               </Link>
             </li>
             <li>
-              <Link to={"about"} className="nav-middle">
-                About
-              </Link>
+              <Link className="nav-middle">About</Link>
             </li>
             <li>
               <Link className="nav-middle">Contact</Link>
@@ -38,7 +88,9 @@ const TopBar = () => {
             </li>
           </ul>
         </div>
-        <div className="flex justify-center cursor-pointer">
+        <div
+          className={enable ? "hidden" : "flex justify-center cursor-pointer"}
+        >
           <Link>
             <img
               className="rounded-full h-10 w-10 object-cover"
